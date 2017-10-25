1904c214959884f85e4df4b5bc4f36b79c9c078b
thenschke <tomasz.henschke@gmail.com>
2017-10-25 22:44:21 +0400

    v0.1.8
    url per env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  ENV['URL_HOST'] = 'http://ec2-35-158-248-197.eu-central-1.compute.amazonaws.com:3003/'
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
