f847c54bddde884ee5e86042ffa435247715e120
thenschke <tomasz.henschke@gmail.com>
2017-10-30 00:03:23 +0400

    v0.3.12

--- app/workers/priceupdate_worker.rb
+++ app/workers/priceupdate_worker.rb
@@ -1,12 +1,12 @@
 class Priceupdate
   include Shoryuken::Worker
   #shoryuken_options queue: ->{ "#{ENV['RAILS_ENV']}_priceupdate" }
-  shoryuken_options queue: "#{Rails.env}_priceupdate", auto_delete: true
+  #shoryuken_options queue: "#{Rails.env}_priceupdate", auto_delete: true
+  shoryuken_options queue: "production_priceupdate", auto_delete: true
 
   def perform(sqs_msg, body)
     id=body
     #puts id
     Price.saveResults(id)
   end
-
  end
