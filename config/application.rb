dbe128b0b2ca850618251eca199abd65e51b880c
thenschke <tomasz.henschke@gmail.com>
2017-10-29 23:43:29 +0400

    v0.3.11
    env in app.rb

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
 
 module Flights
   class Application < Rails::Application
@@ -12,7 +13,7 @@ module Flights
     config.load_defaults 5.1
 
     config.active_job.queue_adapter = :shoryuken
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
