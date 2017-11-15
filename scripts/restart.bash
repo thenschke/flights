b23cbb17acfb7798327123fc258a38bbb2199300
thenschke <tomasz.henschke@gmail.com>
2017-11-15 16:04:14 +0400

    v0.7.2
    fix nginx

--- scripts/restart.bash
+++ scripts/restart.bash
@@ -3,7 +3,7 @@
 # I want to make sure that the directory is clean and has nothing left over from
 # previous deployments. The servers auto scale so the directory may or may not
 # exist.
-sudo kill $(cat /opt/nginx/logs/nginx.pid)
+#sudo kill $(cat /opt/nginx/logs/nginx.pid)
 
 sudo rm -rf /home/ec2-user/flights/tmp
 sudo mkdir /home/ec2-user/flights/tmp
@@ -13,4 +13,4 @@ sudo chmod 777 /home/ec2-user/flights/Gemfile.lock
 
 RAILS_ENV=production bundle exec dotenv shoryuken -R -C /home/ec2-user/flights/config/shoryuken.yml -d -L /home/ec2-user/flights/log/worker.log
 
-sudo /opt/nginx/sbin/nginx
+#sudo /opt/nginx/sbin/nginx
